815e03a96dc0716d4d17cab366accc473b8f84d7
fabianoobispo1 <fabianoobispo@icloud.com>
2023-05-18 01:20:54 -0300

    aula 03

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config'
+
 import fastfy from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastfy()
 
@@ -8,11 +12,16 @@ app.register(cors, {
   origin: true,
 })
 
-app.register(memoriesRoutes)
+app.register(jwt, {
+  secret: 'spacetime',
+})
 
+app.register(memoriesRoutes)
+app.register(authRoutes)
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('HTTP server on👍')
