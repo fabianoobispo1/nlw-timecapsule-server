86e642aeb722880ad0d8423d09a1d45f5f5d0bb0
fabianoobispo1 <fabianoobispo@icloud.com>
2023-05-19 13:20:17 -0300

    aula 04

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,21 @@ import 'dotenv/config'
 import fastfy from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastfy()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -17,6 +28,7 @@ app.register(jwt, {
 })
 
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 app.register(authRoutes)
 app
   .listen({
